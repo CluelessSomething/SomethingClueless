769b2c33fb8d59359450b94e6fd90f83e65b384c
IGotStuckWithDaniel <dcruz0918@gmail.com>
2024-10-15 20:05:23 -0400

    Added Calculator
    While loops. Made it complicated. Liam Murray is mad at me....

--- Calculatour.java
+++ Calculatour.java
@@ -1,28 +1,54 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Calculatour {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your first and second numbers");
-        double a = scanner.nextDouble();
-        double b = scanner.nextDouble();
-        String operator = scanner.next();
-        if (operator.equals("+")) {
-            System.out.println("Your answer is " + (a + b));
-        } else if (operator.equals("-")) {
-            System.out.println("Your answer is " + (a - b));
-        } else if (operator.equals("*")) {
-            System.out.println("Your answer is " + (a * b));
-        } else if (operator.equals("/")) {
-            System.out.println("Your answer is " + (a / b));
+        int timesRannn = 0;
+        double[] results = new double[10];
+        while (timesRannn < 10) {
+            if (timesRannn == 10) {
+                System.out.println(timesRannn);
+                break;
+            }
+            System.out.println("Enter your first and second numbers");
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            System.out.println("Enter your operator");
+            String operator = scanner.next();
+            if (operator.equals("+")) {
+                System.out.println("Your answer is " + (a + b));
+                results[timesRannn] = (a + b);
+            } else if (operator.equals("-")) {
+                System.out.println("Your answer is " + (a - b));
+                results[timesRannn] = (a - b);
+            } else if (operator.equals("*")) {
+                System.out.println("Your answer is " + (a * b));
+                results[timesRannn] = (a * b);
+            } else if (operator.equals("/")) {
+                System.out.println("Your answer is " + (a / b));
+                results[timesRannn] = (a / b);
+            } else if (operator.equals("^")) {
+                System.out.println("Your answer is " + Math.pow(a, b));
+                results[timesRannn] = Math.pow(a, b);
+            } else if (operator.equals("q")) {
+                timesRannn += 1;
+                break;
+            }
+            timesRannn += 1;
         }
-        Math.pow(a, b);
-        String power = scanner.next();
-        if (power.equals("^")) {
-            System.out.println("Enter your number and power");
-            System.out.println("Your answer is " + (a * a));
+        System.out.println("Here are your results");
+        for (double result : results) {
+            System.out.println(result);
+            /// now try running that
         }
-
+        // you NNEEEEDD THE CURLY BRACKET
+        // DANIEL SHUT UP - Liam Muray
+        // Just Run The Program Daniel
+        System.out.println("Here are the amount of times you ran.");
+        System.out.println(timesRannn);
     }
-
 }
+
+// stop at 10 results
+// @ 10 results, stop and print all results
