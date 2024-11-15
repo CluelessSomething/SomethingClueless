74a66e76e6b15f8ed2cc021a6942ff768d575443
CluelessSomething <dcruz0918@gmail.com>
2024-11-15 03:37:12 +0000

    FIXED PLANE I AM AWESOME

--- Plane.java
+++ Plane.java
@@ -1,39 +1,33 @@
 public class Plane extends Vehicle {
-
-    public Plane(String type, String powerType, int numWheels, int numDoors, String color, double fuelEconomy,
-            int numPassangers, int passengerCapacity) {
-        super("Plane", powerType, numWheels, numDoors, color, fuelEconomy, passengerCapacity);
-        boolean islandinggeardown;
-        islandinggeardown = true;
+    private boolean islandinggeardown;
+        public Plane(String type, String powerType, int numWheels, int numDoors, String color, double fuelEconomy,
+                int numPassangers, int passengerCapacity) {
+            super("Plane", powerType, numWheels, numDoors, color, fuelEconomy, numPassangers, passengerCapacity);
+            boolean islandinggeardown;
+            islandinggeardown = true;
+        }
+        public void takeoff() {
+            System.out.println("We have liftoff");
+            islandinggeardown = false;
     }
-
-    public void takeoff() {
-        System.out.println("We have liftoff");
-        islandinggeardown = false;
-    }
-
     public void prepToLand() {
         islandinggeardown = true;
     }
-
     public boolean land() {
-        if (isLandingGearDown) {
+        if (islandinggeardown) {
             System.out.println("We've landed!");
         } else {
             System.out.println("Put the lading gear down!");
             return false;
         }
+                return false;
 
         }
-
         public static void main(String[] args) {
-            Plane cessna = new Plane("Pixie Dust", 6, 2, "Black and Pink Gold", 70, 4);
-            cessa.takeoff();
+            Plane cessna = new Plane("Plane", "Pixie Dust", 6, 2, "Black and Pink Gold", 70, 4, 5);
+            cessna.takeoff();
             cessna.land();
             cessna.prepToLand();
             cessna.land();
-    }
-
-
-    
+    } 
 }
