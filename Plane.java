9a8a9e5b03d4d087719dfd5190aead8796fca9c0
CluelessSomething <dcruz0918@gmail.com>
2024-11-15 02:49:42 +0000

    Fixed Plane

--- Plane.java
+++ Plane.java
@@ -2,11 +2,38 @@ public class Plane extends Vehicle {
 
     public Plane(String type, String powerType, int numWheels, int numDoors, String color, double fuelEconomy,
             int numPassangers, int passengerCapacity) {
-        super("plane", "electric", 3, 2, "black and pretty golf", fuelEconomy, 4, 4);
-
+        super("Plane", powerType, numWheels, numDoors, color, fuelEconomy, passengerCapacity);
+        boolean islandinggeardown;
+        islandinggeardown = true;
     }
-    // Make it a vehicle
-    // Make a variable for whether the landing gear is down
-    // Make a takeoff Function
-    //
+
+    public void takeoff() {
+        System.out.println("We have liftoff");
+        islandinggeardown = false;
+    }
+
+    public void prepToLand() {
+        islandinggeardown = true;
+    }
+
+    public boolean land() {
+        if (isLandingGearDown) {
+            System.out.println("We've landed!");
+        } else {
+            System.out.println("Put the lading gear down!");
+            return false;
+        }
+
+        }
+
+        public static void main(String[] args) {
+            Plane cessna = new Plane("Pixie Dust", 6, 2, "Black and Pink Gold", 70, 4);
+            cessa.takeoff();
+            cessna.land();
+            cessna.prepToLand();
+            cessna.land();
+    }
+
+
+    
 }
